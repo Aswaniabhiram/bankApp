fc650adbcfe18a5e3b6283543e65c0eaddb11388
Aswaniabhiram <aswanik97@gmail.com>
2022-07-06 19:32:32 +0530

    reactive form register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -14,19 +14,23 @@ export class RegisterComponent implements OnInit {
   pswd=""
   //form group
   registerForm=this.fb.group({
-acno:'',
-pswd:'',
-Uname:''
+  
+acno:[''],
+pswd:[''],
+Uname:['',[Validators.required,Validators.pattern('[a-zA-Z ]*')]]
+
   })
   constructor(private ds:DataService,private router:Router,private fb:FormBuilder) {}
     
   ngOnInit(): void {
   }
   register(){
-    var Uname=this.Uname
-    var acno=this.acno
-    var pswd=this.pswd
-    const result=this.ds.register(Uname,acno,pswd)
+    var Uname=this.registerForm.value.Uname
+    var acno=this.registerForm.value.acno
+    var pswd=this.registerForm.value.pswd
+  
+    if(this.registerForm.valid){
+      const result=this.ds.register(Uname,acno,pswd)
     if(result){
       alert("successfully registered")
       this.router.navigateByUrl("")
@@ -34,6 +38,11 @@ Uname:''
     else{
       alert("already existing customer...please log in!!!")
     }
+
+    }
+   else{
+     alert("Invalid Form")
+   } 
   }
 
 }
